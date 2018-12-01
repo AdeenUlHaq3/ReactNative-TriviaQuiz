189124a4fb730c5482106b49ef30a28ab4548a10
adeenulhaq3 <adeen553@gmail.com>
2018-12-01 13:31:27 +0500

    Adding picker

--- screens/Quiz/index.js
+++ screens/Quiz/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { View, Button } from 'react-native';
+import { View, Button, Picker } from 'react-native';
 
 class Quiz extends React.Component {
+    state = {
+        language: null,
+    };
+
     render() {
+        const { language } = this.state;
         const { startQuiz } = this.props.navigation.state.params;
-        console.log(this.props);
         
         return (
             <View>
+                <Picker
+                    selectedValue={language}
+                    style={{ height: 50, width: '100%' }}
+                    onValueChange={(itemValue) => this.setState({ language: itemValue })}>
+                    <Picker.Item label="Java" value="java" />
+                    <Picker.Item label="JavaScript" value="js" />
+                </Picker>
                 <Button
                     onPress={startQuiz}
                     title="Start Quiz"
